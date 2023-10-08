4a52b8c7fb83eb1a76436f9297ba23eb713594ba
Yeansovanvathana <61142466+Yeansovanvathana@users.noreply.github.com>
2023-10-08 13:28:53 +0700

    Update Search feature

--- pages/component/Layout/Navbar.jsx
+++ pages/component/Layout/Navbar.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 
 const Navbar = ({ isTopOfPage, onSearch, buttonActive }) => {
   const [active, setActive] = useState(false);
-  const [searchQuery, setSearchQuery] = useState("");
+  const inputRef = useRef(null);
 
   const handleClick = () => {
     if (buttonActive) {
@@ -11,24 +12,33 @@ const Navbar = ({ isTopOfPage, onSearch, buttonActive }) => {
     }
   };
 
+  const handleClose = () => {
+    setActive(!active);
+    const searchTerm = "";
+    onSearch(searchTerm);
+    inputRef.current.value = "";
+  };
+
   const handleSearch = (e) => {
     const searchTerm = e.target.value;
-    setSearchQuery(searchTerm);
     onSearch(searchTerm); // Pass the search term up to the parent component
   };
 
   return (
     <div>
       <nav
-        className={`z-40 w-full fixed top-2 px-4 py-5${isTopOfPage ? "" : ""}`}
+        className={`z-40 w-full fixed top-2 px-4 py-5 cursor-pointer ${
+          isTopOfPage ? "" : ""
+        }`}
       >
         <div
           className={`flex items-center justify-between mx-auto max-w-screen-xl px-4 md:px-10 py-2 bg-white rounded-full ${
             isTopOfPage ? "" : "border-[1px] border-gray-400"
-          }`}
+          } ${active ? "hidden" : "flex"}`}
+          onClick={handleClick}
         >
           <div
-            onClick={() => (window.location.href = "/")}
+            // onClick={() => (window.location.href = "/")}
             className="flex items-center md:gap-1 cursor-pointer"
           >
             <img
@@ -52,7 +62,6 @@ const Navbar = ({ isTopOfPage, onSearch, buttonActive }) => {
                 } border-2 border-gray-300 rounded-full font-light transition-all ease-in-out duration-500 px-4 focus:outline-none`}
               />
               <button
-                onClick={handleClick}
                 // disabled={buttonActive}
                 className="absolute z-10 -right-[3px] flex bg-primary-50 rounded-full border-2 border-gray-300 w-10 h-10 items-center justify-center"
               >
@@ -60,6 +69,25 @@ const Navbar = ({ isTopOfPage, onSearch, buttonActive }) => {
               </button>
             </div>
           </div>
+        </div>
+        <div
+          className={`items-center justify-between mx-auto max-w-screen-xl py-0.5 bg-white rounded-full ${
+            isTopOfPage ? "" : "border-[1px] border-gray-400"
+          } ${active ? "flex" : "hidden"}`}
+        >
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="Search"
+            onChange={handleSearch}
+            className="w-full p-4 text-xl outline-none rounded-full px-5 md:px-10"
+          />
+          <button className="px-5 md:px-10">
+            <XMarkIcon
+              className="w-7 h-7 text-gray-700"
+              onClick={handleClose}
+            />
+          </button>
         </div>
       </nav>
     </div>
